e513db100c73769909a7a9ac4c09ee847d40e930
Jonathan Kobyluck <jonathan.kobyluck@gmail.com>
2023-05-29 11:06:24 -0400

    initial commit

---
+++ game-night-buddy-app/capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'game-night-buddy-app',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
